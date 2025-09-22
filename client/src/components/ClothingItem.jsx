a3fb44acfce04492d88f03ad786c2938ff68ebd0
lanewells <1180503664+lanewells@users.noreply.github.com>
2025-09-22 10:52:29 -0700

    feat(client-outfits): match html formatting

--- client/src/components/ClothingItem.jsx
+++ client/src/components/ClothingItem.jsx
@@ -2,7 +2,11 @@ export default function ClothingItem({ clothes }) {
   return (
     <div className="clothes-item">
       <h3>Item:</h3>
-      <img src={clothes.imageUrl} alt="" className="clothes-image" />
+      <img
+        src={clothes.imageUrl}
+        alt={clothes.name || ""}
+        className="clothes-image"
+      />
       <div className="clothes-details">
         <p className="name-text">Item name: {clothes.name}</p>
         <p className="color-text">Colors: {clothes.colors}</p>
--- client/src/components/ItemsList.jsx
+++ client/src/components/ItemsList.jsx
@@ -1,9 +1,11 @@
 export default function ItemList({ items, renderItem }) {
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.id || item._id}>{renderItem(item)}</li>
-      ))}
-    </ul>
+    <div className="items-grid">
+      <ul>
+        {items.map((item) => (
+          <li key={item.id || item._id}>{renderItem(item)}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
