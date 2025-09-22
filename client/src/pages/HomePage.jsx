f639d5dd46a47d49d9cc602ff547dd9b14da010c
lanewells <1180503664+lanewells@users.noreply.github.com>
2025-09-22 15:35:25 -0700

    feat(home): HomePage which uses a simple card component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,15 @@
+import HoverCardSimple from "../components/HoverCardSimple"
+
 export default function HomePage() {
-  return <h1>hung-up home page</h1>
+  return (
+    <main id="index-section">
+      <div className="container">
+        <div className="row">
+          <HoverCardSimple title="Clothes" linkedPage="/clothes" />
+          <HoverCardSimple title="Outfits" linkedPage="/outfits" />
+          <HoverCardSimple title="Types" linkedPage="/types" />
+        </div>
+      </div>
+    </main>
+  )
 }
