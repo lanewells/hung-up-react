c4e383dfe41303a4b23a62a202f1e17a176c39c1
lanewells <1180503664+lanewells@users.noreply.github.com>
2025-09-28 12:12:43 -0700

    feat(layout-styling): OutfitItem styles and format

--- client/src/components/OutfitItem.jsx
+++ client/src/components/OutfitItem.jsx
@@ -1,34 +1,26 @@
+import classes from "./OutfitItem.module.scss"
+
 export default function OutfitItem({ outfit }) {
   return (
-    <div className="outfit-item">
-      <img
-        src={outfit.imageUrl}
-        alt={outfit.title || ""}
-        className="outfit-image"
-      />
+    <div className={classes.outfitItem}>
+      <img src={outfit.imageUrl} alt={outfit.title || null} />
       <h3>{outfit.title}</h3>
-      <div className="outfit-details">
-        <div className="outfit-row row"></div>
-        <p className="outfit-text"></p>
+      <div className={classes.outfitDetails}>
+        <div className={classes.outfitRow}></div>
+        <p className={classes.outfitText}></p>
         <p>
           <i className="fas fa-angle-up icon"></i>
         </p>
       </div>
 
-      <p className="occasion-text"></p>
+      <p className={classes.occasionText}></p>
 
       {/* "items" in map below refers to the "items" property of Outfit (clothing items) */}
 
       {outfit.items.map((c) => (
-        <div key={c.id} className="outfit-clothing-images">
-          <div className="clothing-image-wrapper">
-            {c.imageUrl && (
-              <img
-                src={c.imageUrl}
-                alt={c.name || ""}
-                className="clothing-image"
-              />
-            )}
+        <div key={c.id} className={classes.outfitClothingImages}>
+          <div className={classes.clothingImagesWrapper}>
+            {c.imageUrl && <img src={c.imageUrl} alt={c.name || null} />}
           </div>
           <p>{c.name || c.id}</p>
         </div>
