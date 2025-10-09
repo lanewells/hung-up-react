2d53ac51d2472b37950049f9f3faf0f6c17de8cc
lanewells <1180503664+lanewells@users.noreply.github.com>
2025-10-09 14:52:48 -0700

    feat(styles-refine): OutfitsPage styles

--- client/src/components/OutfitItem.jsx
+++ client/src/components/OutfitItem.jsx
@@ -1,30 +1,24 @@
-import classes from "./OutfitItem.module.scss"
+import classes from "../styles/OutfitItem.module.scss"
 
 export default function OutfitItem({ outfit }) {
   return (
     <div className={classes.outfitItem}>
       <img src={outfit.imageUrl} alt={outfit.title || null} />
-      <h3>{outfit.title}</h3>
       <div className={classes.outfitDetails}>
-        <div className={classes.outfitRow}></div>
-        <p className={classes.outfitText}></p>
-        <p>
-          <i className="fas fa-angle-up icon"></i>
-        </p>
+        <div className={classes.row}>
+          <h3>{outfit.title}</h3>
+          <h2>^</h2>
+        </div>
+        <p>Occasions: {outfit.occasion}</p>
         <div className={classes.clothingList}>
           {/* "items" in map below refers to the "items" property of Outfit (clothing items) */}
           {outfit.items.map((c) => (
             <div key={c.id} className={classes.outfitClothingImages}>
-              <div className={classes.clothingImagesWrapper}>
-                {c.imageUrl && <img src={c.imageUrl} alt={c.name || null} />}
-              </div>
-              <p>{c.name || c.id}</p>
+              {c.imageUrl && <img src={c.imageUrl} alt={c.name || null} />}
             </div>
           ))}
         </div>
       </div>
-
-      <p className={classes.occasionText}></p>
     </div>
   )
 }
