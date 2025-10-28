33c5953ca1cdfff99dff312bb0e66f0300c167e5
lanewells <1180503664+lanewells@users.noreply.github.com>
2025-10-27 18:52:11 -0700

    fix(demo): scrollbar layout issue resolved

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { NavLink, Link } from "react-router-dom"
-import DemoBanner from "./DemoBanner"
 import classes from "../styles/Header.module.scss"
 
 export default function Header() {
@@ -7,32 +6,29 @@ export default function Header() {
     isActive ? `${classes.navLink} ${classes.active}` : classes.navLink
 
   return (
-    <>
-      <DemoBanner />
-      <header>
-        <nav>
-          <div className={classes.start}>
-            <Link to="/" className={classes.logo}>
-              <img src="/logo.svg" alt="logo" />
-            </Link>
-            <h2>Hung-Up</h2>
-          </div>
-          <div className={classes.navLinksContainer}>
-            <NavLink className={getClass} to="/" end>
-              My Closet
-            </NavLink>
-            <NavLink className={getClass} to="/clothes">
-              Clothes
-            </NavLink>
-            <NavLink className={getClass} to="/outfits">
-              Outfits
-            </NavLink>
-            <NavLink className={getClass} to="/drawers">
-              Drawers
-            </NavLink>
-          </div>
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav>
+        <div className={classes.start}>
+          <Link to="/" className={classes.logo}>
+            <img src="/logo.svg" alt="logo" />
+          </Link>
+          <h2>Hung-Up</h2>
+        </div>
+        <div className={classes.navLinksContainer}>
+          <NavLink className={getClass} to="/" end>
+            My Closet
+          </NavLink>
+          <NavLink className={getClass} to="/clothes">
+            Clothes
+          </NavLink>
+          <NavLink className={getClass} to="/outfits">
+            Outfits
+          </NavLink>
+          <NavLink className={getClass} to="/drawers">
+            Drawers
+          </NavLink>
+        </div>
+      </nav>
+    </header>
   )
 }
