3d2b0bcade63c788025a2f722b13be06ed150a16
lanewells <1180503664+lanewells@users.noreply.github.com>
2025-11-11 21:43:31 -0800

    fix(mobile): clothing item hover text adjusted to be responsive

--- client/src/components/ClothingItem.jsx
+++ client/src/components/ClothingItem.jsx
@@ -6,15 +6,17 @@ export default function ClothingItem({ clothing }) {
       <img src={clothing.imageUrl} alt={clothing.name || null} />
       <div className={classes.details}>
         <p className={classes.nameText}>{clothing.name}</p>
-        <p>
-          <span>Colors:</span> {clothing.colors}
-        </p>
-        <p>
-          <span>Subtype:</span> {clothing.subtype}
-        </p>
-        <p>
-          <span>Type:</span> {clothing.typeName}
-        </p>
+        <div className={classes.specs}>
+          <p>
+            <span>Colors:</span> {clothing.colors}
+          </p>
+          <p>
+            <span>Subtype:</span> {clothing.subtype}
+          </p>
+          <p>
+            <span>Type:</span> {clothing.typeName}
+          </p>
+        </div>
       </div>
     </div>
   )
